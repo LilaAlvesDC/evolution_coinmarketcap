f92be5432edc12d4e4e78a46dccc66bf2c0c3f32
LilaAlvesDC <106811176+LilaAlvesDC@users.noreply.github.com>
2022-06-11 16:09:14 -0300

    Add files via upload

--- Scraping/Scraping.py
+++ Scraping/Scraping.py
@@ -1,50 +1,42 @@
-from email.mime import text
-from gettext import textdomain
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 import requests
-import pandas as pd
-import numpy as np 
-from time import sleep
-from random import randint
-from bs4 import NavigableString
 
-website = ['https://coinmarketcap.com/es/historical/20130428/','https://coinmarketcap.com/es/historical/20130505/']
+website = ['https://coinmarketcap.com/es/historical/20130428/']
 
-# result = requests.get(website)
-# content = result.text 
-# soup = BeautifulSoup(content, 'lxml')
-
-# print(soup.prettify())
-
-Row = [ ]
-Name = [ ]
-MarketCap = [ ]
-Month = [ ]
-Year = [ ]
+# tags del website 
 
 tag_rank = 'cmc-table__cell cmc-table__cell--sticky cmc-table__cell--sortable cmc-table__cell--left cmc-table__cell--sort-by__rank'
-tag_name = 'cmc-table__column-name--name cmc-link'
+tag_name = 'sc-1ibw5f9-0 bpOMHJ cmc-table__column-name cmc-table__column-name--narrow-layout'
 tag_marketcap = 'cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__market-cap'
 tag_year = 'sc-1v5cdrp-0 jaLaYr cmc-historical-detail__title'
 
 for cripto in website: 
     data = requests.get(cripto)
-    soup = BeautifulSoup(data.content, 'html.parser') # or 'lxml' 'html.parser'
+    soup = BeautifulSoup(data.content, 'html.parser') 
 
     # find_all names
-    names = soup.find_all('a', {'class': tag_name})
-    for names in names:
-        Name = [ ]
-        Name.append(names.text)
-        
+    names = soup.find_all('div', {'class': tag_name})
+    Name = [x.find('a').get('title') for x in names]
+    print(Name)
+
     # find_all MarketCap
     marketscaps = soup.find_all('td', {'class': tag_marketcap})
     marketscaps_amount = [x.find('div').text for x in marketscaps]
     print(marketscaps_amount)
-   
-# with open('CoinMarketCap.txt', 'w') as file: 
-#    file.write(Crypto) 
 
+    # find_all Year
+    years = soup.find('h1').text[-4:]
+    for year in years:
+        Year = [ ]
+        Year.append(years)
 
+    # find_all Month  
+    months = soup.find('h1').text[20:23]
+    for month in months:
+        Month = [ ]
+        Month.append(months)
+    print(Year)  
+    print(Month)  
 
-
+with open('CoinMarketCap.txt', 'w') as file: 
+    file.write(str(Name)) 
